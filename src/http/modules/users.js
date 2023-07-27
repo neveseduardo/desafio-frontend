7ff7628f23d9d7dfb80bad602dfa59b952113f9f
Eduardo Neves <eduardo.neves@oliveiratrust.com.br>
2023-07-27 08:22:49 -0300

    feat: limpesa dos arquivos e pastas desnecessarias

--- src/http/modules/users.js
+++
@@ -1,8 +0,0 @@
-import axios from '@/http/interceptor'
-
-export default {
-	getUsers: query => axios.get('/users?_sort=id&_order=desc' + query),
-	storeUser: data => axios.post('/users', data),
-	deleteUser: id => axios.delete(`/users/${id}`),
-	updateUser: (data, id) => axios.put(`/users/${id}`, data),
-}
--- src/layouts/HomeLayout/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <footer class="home-footer">
-        <div class="container">Fintools | Desafio Front end</div>
-    </footer>
-</template>
